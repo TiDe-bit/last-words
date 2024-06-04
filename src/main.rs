007099d3c05595e8eb07e67302c22e9271c4c30d
Tim Dellmann <tdellmann@ente.cool>
2024-06-04 22:16:54 +0200

    maybe fix these pescy crashes on weird container names

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use anyhow::{Error, Ok};
 use bollard::{container::LogsOptions, Docker};
-use core::time::Duration;
+use core::{panic, time::Duration};
 use futures::stream::StreamExt;
-use std::{
-    env,
-    io::{stdout, Write},
-    ops::Add,
-};
+use std::{env, ops::Add};
 use tokio::time;
 
 mod undertaker;
@@ -17,13 +13,21 @@ const DAY: Duration = Duration::from_secs(86400);
 async fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() >= 2);
-    println!("last words for container {:?}", args[1]);
+    println!("last words for docker container {}", args[1]);
     let docker = Docker::connect_with_socket_defaults();
     let container_name = crate::undertaker::get_full_container_name(args[1].clone())
         .await
         .unwrap();
+    println!("resolved container name {}", container_name);
+
+    let pruned_container_name = container_name.strip_prefix("/").or(Some(&container_name));
+
+    if pruned_container_name.is_none() {
+        panic!("couldn't actually resolve container name. wtf.");
+    }
+
     let log_stream = docker.unwrap().logs(
-        container_name.strip_prefix("/").unwrap(),
+        pruned_container_name.unwrap(),
         Some(LogsOptions::<String> {
             follow: true,
             stdout: true,
@@ -34,10 +38,11 @@ async fn main() -> Result<(), Error> {
             until: chrono::Utc::now().add(DAY).timestamp(),
         }),
     );
-    time::sleep(Duration::from_millis(10)).await;
+
+    let _ = time::sleep(Duration::from_millis(10));
     let mut stream = log_stream;
     while let Some(item) = stream.next().await {
-        stdout().write_all(item?.as_ref()).unwrap();
+        println!("{}", item.unwrap())
     }
 
     Ok(())
